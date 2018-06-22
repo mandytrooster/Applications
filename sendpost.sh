e4d041dbf07e0996672446509366288406db8e09
mandyMac <mandytrooster@gmail.com>
2018-06-22 16:41:34 +0200

    fixed it?

--- sendpost.sh
+++ sendpost.sh
@@ -1,4 +1,4 @@
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"name": "postname","score":"1"}' \
-  http://unityanalyticstest.herokuapp.com/newscore
+  http://highscoreapplications.herokuapp.com/newscore
